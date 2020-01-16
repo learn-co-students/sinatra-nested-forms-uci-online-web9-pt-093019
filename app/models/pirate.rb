4c6f86ec38f5865c5661b9ba2406b002cbba9b04
Alice Freda <alicefreda@gmail.com>
2020-01-16 11:37:27 -0800

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize(attributes)
+        attributes.each do |k,v|
+            self.send("#{k}=", v) if self.respond_to?(k)
+          end
+          self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+    @@all = []
+
+    def initialize(attributes)
+        attributes.each do |k,v|
+            self.send("#{k}=", v) #if self.respond_to?(k)
+        end
+        self.save
+    end
+
+    def self.all
+        @all
+    end
+
+    def self.clear
+        @@all = []
+    end
+
+    def save
+        @@all << self
+    end
+
 end
