3140eb56e9216357756f08e9056326435418b912
Justin Phillips <justinphil02@yahoo.com>
2020-01-19 02:31:16 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,12 @@
 class Pirate
+  @@all = []
+  attr_accessor :name, :weight, :height
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  @@all = []
+  attr_accessor :name, :type, :booty
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
 end
